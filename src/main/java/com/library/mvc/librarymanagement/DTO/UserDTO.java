d96fcd2033931bc7de741c399d21dcf597a71098
tranthienquang05 <tranthienquang05@gmail.com>
2026-07-23 20:15:13 +0700

    part2

--- src/main/java/com/library/mvc/librarymanagement/DTO/UserDTO.java
+++ src/main/java/com/library/mvc/librarymanagement/DTO/UserDTO.java
@@ -11,79 +11,22 @@ public class UserDTO {
 
     private String id;
 
-    @NotBlank(message = "Username is required")
-    @Size(min = 4, max = 20,
-            message = "Username must be between 4 and 20 characters")
+    @NotBlank
+    @Size(min=4,max=20)
     private String username;
 
-    @NotBlank(message = "Password is required")
-    @Size(min = 6,
-            message = "Password must contain at least 6 characters")
+    @NotBlank
+    @Size(min=6)
     private String password;
 
-    @NotBlank(message = "Full name is required")
-    @Size(min = 5, max = 50,
-            message = "Full name must be between 5 and 50 characters")
+    @NotBlank
+    @Size(min=5,max=50)
     private String fullName;
 
-    @NotBlank(message = "Role is required")
+    @NotBlank
     private String role;
 
-    @NotBlank(message = "Status is required")
+    @NotBlank
     private String status;
 
-    public UserDTO() {
-    }
-
-    public UserDTO(String id) {
-        this.id = id;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
